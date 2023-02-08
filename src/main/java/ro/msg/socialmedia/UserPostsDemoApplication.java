759ce15b32a459d2a7923e6396cbadf96bc4b9ba
Chris <ateamcristian@yahoo.de>
2023-02-08 18:50:21 +0200

    empty first commit

---
+++ src/main/java/ro/msg/socialmedia/UserPostsDemoApplication.java
@@ -0,0 +1,13 @@
+package ro.msg.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPostsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPostsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/msg/socialmedia/UserPostsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.msg.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPostsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
