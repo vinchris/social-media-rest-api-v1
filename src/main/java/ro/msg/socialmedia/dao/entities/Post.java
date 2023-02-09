d300cb8afa7e37393d14071e66611e46c323a6fc
Chris <ateamcristian@yahoo.de>
2023-02-09 13:32:10 +0200

    confirgured post entity

---
+++ src/main/java/ro/msg/socialmedia/dao/entities/Post.java
@@ -0,0 +1,28 @@
+package ro.msg.socialmedia.dao.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "posts")
+@Table(name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+}
